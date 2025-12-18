8deb9cb6063de392bdbed013ceeae7b7ecd8ab12
Deyved Antonio <deyved.antonio@gmail.com>
2025-12-18 12:23:28 -0300

    feat: registar o admin de brand e alterar o campo do admin de car.

--- cars/admin.py
+++ cars/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Brand, Car
 
 
 class CarAdmin(admin.ModelAdmin):
 
-    list_display = ['model', 'brand', 'factory_year', 'model_year', 'value']
+    list_display = ['model', 'brand_id', 'factory_year', 'model_year', 'value']
     search_fields = ['model']
 
 
 admin.site.register(Car, CarAdmin)
+
+
+class BrandAdmin(admin.ModelAdmin):
+
+    list_display = ['name',]
+    search_fields = ['name']
+
+
+admin.site.register(Brand, BrandAdmin)
